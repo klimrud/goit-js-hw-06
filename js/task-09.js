5b3541bb74da557ba08a253535008823ef4223fa
Tetiana Klimenko <ktlimenko@gmail.com>
2023-01-15 01:14:32 +0200

    dz-06-number9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,24 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const widgetEl = document.querySelector('.widget');
+const colorEl = document.querySelector('.color')
+const btnChangeColorEl = document.querySelector('.change-color')
+
+
+// console.log(widgetEl);
+//  console.log(colorEl);
+// console.log(btnChangeColorEl);
+
+btnChangeColorEl.addEventListener('click', onChangeColor);
+
+function onChangeColor (event) {
+  // console.log('click')
+  document.body.style.backgroundColor = [getRandomHexColor()];
+ colorEl.textContent = [getRandomHexColor()];
+ console.log('span.color:',colorEl.textContent);
+  // console.log(document.body.style.backgroundColor);
+  // console.log(getRandomHexColor(16777215));
+}
