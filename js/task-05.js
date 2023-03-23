d6ede0e34d151093bfa8b1e6a1500d263626f1f0
Tetiana Klimenko <ktlimenko@gmail.com>
2023-03-23 02:03:07 +0200

    statistics

--- js/task-05.js
+++ js/task-05.js
@@ -3,15 +3,15 @@
 
  const inputEl = document.querySelector("#name-input");
  const outputEl = document.querySelector('#name-output');
-//  console.log(outputEl.textContent);
+  console.log(outputEl.textContent);
  inputEl.addEventListener("input", onInputChange);
 
 
 
 function onInputChange(event) {
   outputEl.textContent = event.currentTarget.value ? event.currentTarget.value : 'Anonymous'
-    // console.log(outputEl);
-  // console.log(inputEl.value);
+    console.log(outputEl);
+  console.log(inputEl.value);
  
 };
 
